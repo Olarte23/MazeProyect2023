8236c9c79b82b8e28c27a684cef48d48cefcc4fc
CRISTIAN OLARTE CARRASQUILLA <cristianolarte321414@correo.itm.edu.co>
2023-04-15 10:05:52 -0500

    The last details

--- Maze.UI/Program.cs
+++ Maze.UI/Program.cs
@@ -8,11 +8,18 @@ try
 	//maze.FillWay();
 	//Console.WriteLine(maze);
 
-    var maze = new MazeOp(20,40);
+    var maze = new MazeOp(20,20);
+    Console.WriteLine("\n**** Maze Proyect ****\n");
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine(maze);
+    Console.ForegroundColor = ConsoleColor.Black;
     Console.WriteLine("\nWay to Exit\n");
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.ForegroundColor = ConsoleColor.Blue;
     maze.FillWay();   
     Console.WriteLine(maze);
+    Console.BackgroundColor = ConsoleColor.Black;
 }
 catch (Exception ex)
 {
